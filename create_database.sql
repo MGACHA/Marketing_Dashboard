6481fe5418a2e450da322cdd54595c6215fe695e
MGACHA <71405661+MGACHA@users.noreply.github.com>
2026-03-04 12:27:01 +0000

    create_database.sql
    SQL script for dba creation and  removal

---
+++ create_database.sql
@@ -0,0 +1,19 @@
+-- Create database
+CREATE DATABASE marketing;
+GO
+
+-- Switch to database
+USE marketing;
+GO
+
+
+/*-- DROP database
+USE master;
+GO
+
+ALTER DATABASE marketing SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+DROP DATABASE marketing;
+GO
+*/
